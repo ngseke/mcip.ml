6ddbcd4a1d97f5d4ca09d78fe1beb743c5fc3ddf
Huang Xingqiao <a92304a92304@gmail.com>
2021-10-31 18:58:58 +0800

    feat: 更新 Jumbotron

--- components/Jumbotron.vue
+++ components/Jumbotron.vue
@@ -1,24 +1,30 @@
 <template lang="pug">
 header(:style='headerStyle' :class='headerClass')
   slot
-    .container: .row.no-gutters.justify-content-around
+    .container: .row.justify-content-start.align-items-center
       .col-12.col-sm-10.col-md-8.col-lg-6.col-xl-5
         .logo(:class='logoClass')
           Logo(alt='樂台計畫: 大專院校音樂賽事平台')
-        //- span.ad #[b 樂台計畫]現已更新至全新風貌!
-      .col.col-xl-5
-        h2.text-center.text-md-left
-          | 即刻透過 LINE
-          br
-          | 加入#[strong 樂台計畫]！
+      .col-12.col-lg.position-relative
+        .text-center.text-lg-left
+          h2
+            | 凝聚並點燃熱情溫度的
+            br
+            | 音樂賽事大平台
+          .mt-3
+            Hashtags
+        //- .circle
+          img(src='https://images.unsplash.com/photo-1514525253161-7a46d19cd819?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y29uY2VydHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60')
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
+import Hashtags from '~/components/Hashtags.vue'
 
 export default {
   components: {
     Logo,
+    Hashtags,
   },
   props: {
     isAnimation: {
@@ -67,12 +73,15 @@ export default {
 @import '~assets/sass/logo'
 
 header
+  overflow-x: hidden
   position: relative
   background: url('~assets/img/bg.jpg') center center / cover
+  background-color: nth($secondary-list, 3)
   color: white
   min-height: 100vh
+  +py(3rem)
   @media (max-width: 767.98px)
-    min-height: 460px
+    min-height: 480px
   +flex-center
   flex-direction: column
   &.dense
@@ -85,13 +94,14 @@ header
 
   h2
     color: rgba(white, .8)
-    text-align: center
-    font-size: 2.25rem
-    font-weight: 500
+    font-size: 2.5rem
+    font-weight: 900
     letter-spacing: 1px
     line-height: 1.4
     +gradient-text(linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%))
     opacity: .95
+    @media (max-width: 767.98px)
+      font-size: 2rem
 
   .ad
     display: inline-block
@@ -101,4 +111,33 @@ header
     margin-bottom: 1rem
     b
       font-weight: bold
+
+.circle
+  $size: 40rem
+  position: relative
+  transform: translate(2rem, 2rem)
+  +wh($size)
+  img
+    +wh(100%)
+    object-fit: cover
+    z-index: 1
+    border-radius: 100rem
+    position: relative
+  &::after
+    $offset: 1.5rem
+    $dot-size: 20%
+    $dot-color: nth($secondary-list, 1)
+    content: ''
+    background-image: radial-gradient($dot-color $dot-size, transparent 11%), radial-gradient($dot-color $dot-size, transparent 11%)
+    background-size: 20px 20px
+    background-position: 0 0, 30px 30px
+    background-repeat: repeat
+    +wh($size)
+    border-radius: 100rem
+    z-index: 0
+    position: absolute
+    left: 0
+    top: 0
+    transform: translate(-$offset, $offset)
+    opacity: .7
 </style>
