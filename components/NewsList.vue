57064eb99ad65118bcc2d5847ba91f5bb1252e24
Huang Xingqiao <a92304a92304@gmail.com>
2021-11-02 21:06:01 +0800

    feat: 懶載入快訊列表

--- components/NewsList.vue
+++ components/NewsList.vue
@@ -1,9 +1,12 @@
 <template lang="pug">
 ul.article-list
   li(v-for='i in list')
-    a(:href='`/news/${i.id}`')
-      h2 {{ i.title }}
+    nuxt-link(:to='`/news/${i.id}`')
+      .title {{ i.title }}
       small {{ convertTime(i.timestamp) }}
+  li(ref='loadMore' v-if='!isEnd')
+    fa.icon(icon='circle-notch' spin v-if='isLoading')
+    a(href='#' @click.prevent='loadMore' v-else) 載入更多
 </template>
 
 <script>
@@ -15,10 +18,36 @@ export default {
       default: null,
       type: Array,
     },
+    isEnd: {
+      default: false,
+      type: Boolean,
+    },
+    isLoading: {
+      default: false,
+      type: Boolean,
+    },
+  },
+  mounted () {
+    this.observeLoadMore()
   },
   methods: {
     convertTime (_) {
       return dayjs(_).format('YYYY年MM月DD日')
+    },
+    loadMore () {
+      if (this.isLoading) return
+      this.$emit('loadMore')
+    },
+    observeLoadMore () {
+      const target = this.$refs.loadMore
+      const observer = new IntersectionObserver(() => {
+        this.loadMore()
+      }, { threshold: 1 })
+      observer.observe(target)
+
+      this.$once('hook:beforeDestroy', function () {
+        observer.disconnect()
+      })
     },
   },
 }
@@ -30,14 +59,14 @@ ul.article-list
   padding: 0
   li
     padding: 1rem
-    font-size: 1.5rem
+    font-size: 1.3rem
     a
       display: block
       +floating-link
-      h2
+      .title
         padding-bottom: .25rem
+        font-weight: 500
         margin: 0
-        font-size: 1.3rem
         color: transparent
         background-clip: text
         -webkit-background-clip: text
@@ -45,6 +74,10 @@ ul.article-list
       small
         font-size: .7
         color: rgba(#666, 1)
+      &.load-more
   li:not(:last-child)
     border-bottom: solid 1px rgba(#ccc, .5)
+
+.icon
+  color: $black
 </style>
