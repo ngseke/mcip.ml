5b47cba61aa3e759b1b44b3eedb3ad3795e6a9e4
Huang Xingqiao <a92304a92304@gmail.com>
2021-11-02 17:51:47 +0800

    feat: 調整插圖樣式

--- components/IndexSectionIntroduction.vue
+++ components/IndexSectionIntroduction.vue
@@ -1,33 +1,42 @@
 <template lang="pug">
 section: .container
   .row.justify-content-around
-    .col-12.col-md-3.col-lg-3.order-2.order-md-1
+    .col-12.col-md-5.col-lg-5.order-2.order-md-1.position-relative
       div(:class='peepClass')
       .screenshot-area
-        img.mockup-1.ml-lg-5(v-if='type === 1' src='~/assets/img/screenshot/mockup-line-app.png' alt='Line App 畫面截圖' v-scroll-reveal.reset={ scale: .9, opacity: 1 })
-        img.mockup-2(v-else src='~/assets/img/screenshot/mockup-backstage.png' alt='管理後台截圖' v-scroll-reveal.reset={ scale: .9, opacity: 1 })
+        img.mockup-line-app(
+          v-if='type === 1'
+          src='~/assets/img/screenshot/mockup-line-app.png'
+          alt='Line App 畫面截圖'
+        )
+        img.mockup-backstage(
+          v-if='type === 2'
+          src='~/assets/img/screenshot/mockup-backstage.png'
+          alt='管理後台截圖'
+        )
 
-    .col-12.col-md-8.col-lg-6.order-1.order-md-2
-      .align-center-article
-        RoleSwitcher(v-model='type')
-        template(v-if='type === 1')
-          h3 透過樂台計畫#[br]3 分鐘即完成報名
-          p.mb-3
-            | 不需額外下載 App，用 LINE 就能立刻加入
+    .col-12.col-md-7.col-lg-6.order-1.order-md-2
+      RoleSwitcher(v-model='type')
+      template(v-if='type === 1')
+        h3 透過樂台計畫#[br]3 分鐘即完成報名
+        p.mb-3
+          | 不需額外下載 App，用 LINE 就能立刻加入
 
-          .mb-4.d-flex.flex-column.d-md-block
-            nuxt-link.gradient-btn.line(target='_blank' to="/line") 加入 LINE 官方帳號
-          .d-none.d-md-inline-block
-            img.qrcode(src='~/assets/img/line-app-qrcode-shorthand.png' alt='樂台計畫 LINE App QRCode')
-
-        template(v-else)
-          h3 為音樂賽事量身打造的#[br]解決方案
-          ul.pl-4
-            li 眾多賽事齊聚一堂，大幅增加活動曝光
-            li 金流代收服務，即時向參賽者推播繳費結果
-            li 跨平台系統，隨時隨地掌握報名狀況
-          .pl-1.mb-3
-            a(href='https://manage.mcip.ml/' target='_blank') 前往社團管理後台 #[fa.mx-1(icon='external-link-alt')]
+        .mb-4.d-flex.flex-column.d-md-block
+          nuxt-link.gradient-btn.line(target='_blank' to="/line") 加入 LINE 官方帳號
+        .d-none.d-md-inline-block
+          img.qrcode(
+            src='~/assets/img/line-app-qrcode-shorthand.png'
+            alt='樂台計畫 LINE App QRCode'
+          )
+      template(v-if='type === 2')
+        h3 為音樂賽事量身打造的#[br]解決方案
+        ul.pl-4
+          li 眾多賽事齊聚一堂，大幅增加活動曝光
+          li 金流代收服務，即時向參賽者推播繳費結果
+          li 跨平台系統，隨時隨地掌握報名狀況
+        .pl-1.mb-3
+          a(href='https://manage.mcip.ml/' target='_blank') 前往社團管理後台 #[fa.mx-1(icon='external-link-alt')]
 </template>
 
 <script>
@@ -39,9 +48,11 @@ export default {
   },
   computed: {
     peepClass () {
+      const { type } = this
+
       return {
-        'peep-1 d-block d-md-none d-lg-block': this.type === 1,
-        'peep-2 d-none d-lg-block ': this.type === 2,
+        'peep-1 d-block d-md-none d-lg-block': type === 1,
+        'peep-2 d-none d-lg-block': type === 2,
       }
     },
   },
@@ -59,60 +70,54 @@ section
   max-width: 7rem
   margin-right: 2rem
 
-.align-center-article
-  +flex-center
-  height: 100%
-  align-items: stretch
-  justify-content: flex-start
-
 .peep-1
   position: absolute
-  left: -11rem
+  left: -9rem
   bottom: -4.5rem
   background: center / contain no-repeat url('~assets/img/peep/man-with-phone.svg')
   +wh(16rem, 100%)
   @include media-breakpoint-down(sm)
     +wh(12rem, 100%)
     left: 0rem
-  @media (max-width: 470px)
-    +wh(11rem, 100%)
-    left: -2rem
 
 .peep-2
   position: absolute
-  left: -13rem
+  left: -11rem
   bottom: -9rem
   background: center / contain no-repeat url('~assets/img/peep/man-using-notebook.svg')
   +wh(21rem, 30rem)
+  @include media-breakpoint-down(lg)
+    left: -13rem
   @include media-breakpoint-down(sm)
     +wh(14rem, 100%)
     left: 0rem
 
 .screenshot-area
-  .mockup-1
-    @include media-breakpoint-down(sm)
+  position: relative
+  display: flex
+  justify-content: center
+
+  img
+    +wh(100%, auto)
+    &.mockup-line-app
       max-width: 15rem
+      @include media-breakpoint-down(sm)
+        transform: translateX(30%)
 
-    @media (max-width: 400px)
-      margin-left: 5rem
+    &.mockup-backstage
+      height: 16rem
+      width: auto
+      max-width: none
+      margin-left: 0
+      @include media-breakpoint-down(lg)
+        margin-left: -2rem
 
-  .mockup-2
-    height: 16rem
-    width: auto
-    max-width: none
-    margin-left: 0
-    @include media-breakpoint-down(lg)
-      margin-left: -2rem
+      @include media-breakpoint-down(md)
+        height: 9rem
 
-    @include media-breakpoint-down(md)
-      height: 9rem
-
-    @include media-breakpoint-down(sm)
-      max-width: 20rem
-      height: auto
-
-    @media (max-width: 400px)
-      height: auto
+      @include media-breakpoint-down(sm)
+        max-width: 20rem
+        height: auto
 
 ul
   li
