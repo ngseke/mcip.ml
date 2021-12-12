28b82fa34666c287b6c1dcea237051912bd20327
Huang Xingqiao <a92304a92304@gmail.com>
2021-12-12 16:16:28 +0800

    feat: Add generic for `fetch()`

--- modules/static-data.ts
+++ modules/static-data.ts
@@ -2,8 +2,8 @@ import axios from 'axios'
 import join from 'url-join'
 import { STATIC_DATA_URL } from './config'
 
-export async function fetch (name: string) {
+export async function fetch <T = any> (name: string) {
   const url = join(STATIC_DATA_URL, name)
-  const { data } = await axios.get(url)
+  const { data } = await axios.get<T>(url)
   return data
 }
