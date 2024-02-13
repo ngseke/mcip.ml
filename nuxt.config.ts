490785bbec6152c58fff8b3cc195c8698ae08b57
ngseke <ngseke@gmail.com>
2024-02-13 16:34:50 +0800

    fix: fix font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -53,7 +53,9 @@ export default defineNuxtConfig({
       ],
       link: [
         { rel: 'icon', type: 'image/png', href: '/favicon.png' },
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Sans+TC:400,500,700,900|Overpass:400,400i,600,700,900&display=swap&subset=chinese-traditional' },
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100..900&family=Overpass:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap' },
       ],
       script: [
         { src: 'https://www.googletagmanager.com/gtag/js?id=UA-134521649-1', async: true },
