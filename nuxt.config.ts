5cf8e9be9a63b23513d7f976eef148a204f4a04e
ngseke <ngseke@gmail.com>
2024-02-14 18:07:51 +0800

    feat: update meta viewport content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -45,7 +45,7 @@ export default defineNuxtConfig({
       },
       meta: [
         { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' },
+        { name: 'viewport', content: 'width=device-width,initial-scale=1' },
         { name: 'author', content: 'ngseke' },
         { hid: 'description', name: 'description', content: description },
         { hid: 'og:description', property: 'og:description', content: description },
